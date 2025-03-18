e4ccd45571ef620f559ae04dbbc3411f4d0651a1
santjonatas <jonatascaina100@gmail.com>
2025-03-17 21:26:09 -0300

    adicionando seed de person admin

--- src/main/java/jonatasSantos/royalLux/infra/data/DatabaseSeeder.java
+++ src/main/java/jonatasSantos/royalLux/infra/data/DatabaseSeeder.java
@@ -1,13 +1,15 @@
 package jonatasSantos.royalLux.infra.data;
 
+import jonatasSantos.royalLux.core.application.contracts.repositories.PersonRepository;
 import jonatasSantos.royalLux.core.application.contracts.repositories.UserRepository;
+import jonatasSantos.royalLux.core.domain.entities.Person;
 import jonatasSantos.royalLux.core.domain.entities.User;
 import jonatasSantos.royalLux.core.domain.valueobjects.UserRole;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-import java.util.List;
+import java.time.LocalDate;
 
 @Component
 public class DatabaseSeeder implements CommandLineRunner {
@@ -18,11 +20,28 @@ public class DatabaseSeeder implements CommandLineRunner {
     @Value("${admin.password}")
     private String adminPassword;
 
+    @Value("${admin.name}")
+    private String adminName;
+
+    @Value("${admin.birth}")
+    private String adminBirth;
+
+    @Value("${admin.cpf}")
+    private String adminCpf;
+
+    @Value("${admin.phone}")
+    private String adminPhone;
+
+    @Value("${admin.email}")
+    private String adminEmail;
+
     private final UserRepository userRepository;
+    private final PersonRepository personRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public DatabaseSeeder(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public DatabaseSeeder(UserRepository userRepository, PersonRepository personRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.personRepository = personRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
@@ -36,9 +55,12 @@ public class DatabaseSeeder implements CommandLineRunner {
     }
 
     public void createAdmin(){
-        User admin = new User(adminUsername, UserRole.ADMIN, true);
-        admin.validatePassword(adminPassword);
-        admin.setPassword(passwordEncoder.encode(adminPassword));
-        userRepository.save(admin);
+        User userAdmin = new User(this.adminUsername, UserRole.ADMIN, true);
+        userAdmin.validatePassword(this.adminPassword);
+        userAdmin.setPassword(passwordEncoder.encode(this.adminPassword));
+        userRepository.save(userAdmin);
+
+        Person personAdmin = new Person(userAdmin, this.adminName, LocalDate.parse(this.adminBirth), this.adminCpf, this.adminPhone, this.adminEmail);
+        personRepository.save(personAdmin);
     }
 }
