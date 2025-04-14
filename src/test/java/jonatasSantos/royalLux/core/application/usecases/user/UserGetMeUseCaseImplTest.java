c8c36ff90b33fcf7f3e29ce1568e23314fd2d8dc
Jônatas Santos <jonatassantos.code@gmail.com>
2025-04-14 09:54:52 -0300

    teste get me usecase pronto git push

--- src/test/java/jonatasSantos/royalLux/core/application/usecases/user/UserGetMeUseCaseImplTest.java
+++ src/test/java/jonatasSantos/royalLux/core/application/usecases/user/UserGetMeUseCaseImplTest.java
@@ -2,6 +2,7 @@ package jonatasSantos.royalLux.core.application.usecases.user;
 
 import jakarta.persistence.EntityNotFoundException;
 import jonatasSantos.royalLux.core.application.contracts.repositories.UserRepository;
+import jonatasSantos.royalLux.core.application.models.dtos.user.UserGetMeUseCaseOutputDto;
 import jonatasSantos.royalLux.core.domain.entities.User;
 import jonatasSantos.royalLux.core.domain.enums.UserRole;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,6 +12,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -33,15 +35,37 @@ class UserGetMeUseCaseImplTest {
     @DisplayName("Quando não existir usuário com o mesmo id, estourar exceção EntityNotFoundException com mensagem 'Usuário inexistente'")
     void execute() {
         // Arrange
-        User userLooged = new User("joao_1", UserRole.CLIENT, true);
+        User userLogged = new User("joao_1", UserRole.CLIENT, true);
 
-        when(userRepository.findById(String.valueOf(userLooged.getId()))).thenReturn(Optional.empty());
+        when(userRepository.findById(String.valueOf(userLogged.getId()))).thenReturn(Optional.empty());
 
         // Act + Assert
         EntityNotFoundException exception = assertThrows(EntityNotFoundException.class, () -> {
-            userGetMeUseCase.execute(userLooged);
+            userGetMeUseCase.execute(userLogged);
         });
 
         assertEquals("Usuário inexistente", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Deve retornar usuário com sucesso")
+    void deveBuscarUsuarioComSucesso() {
+        // Arrange
+        User userLogged = new User("joao_1", UserRole.CLIENT, true);
+        userLogged.setId(10);
+        userLogged.setUpdatedAt(LocalDateTime.of(2025, 4, 10, 12, 0));
+
+        when(userRepository.findById(String.valueOf(userLogged.getId()))).thenReturn(Optional.of(userLogged));
+
+        // Act
+        UserGetMeUseCaseOutputDto output = userGetMeUseCase.execute(userLogged);
+
+        // Assert
+        assertEquals(userLogged.getId(), output.id());
+        assertEquals(userLogged.getUsername(), output.username());
+        assertEquals(userLogged.getRole(), output.role());
+        assertEquals(userLogged.isActive(), output.active());
+        assertEquals(userLogged.getCreatedAt(), output.createdAt());
+        assertEquals(userLogged.getUpdatedAt(), output.updatedAt());
     }
 }
