5d5586cdb97ce5a0c6887eb0b3b1d141565fe8b7
Jônatas Santos <jonatas.santana@qwasolucoes.com.br>
2025-06-12 11:55:04 -0300

    testes em role entity

--- src/test/java/jonatasSantos/royalLux/core/domain/entities/RoleTest.java
+++ src/test/java/jonatasSantos/royalLux/core/domain/entities/RoleTest.java
@@ -3,6 +3,8 @@ package jonatasSantos.royalLux.core.domain.entities;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RoleTest {
@@ -21,5 +23,52 @@ class RoleTest {
         assertEquals("Nome não pode ser nulo", exception.getMessage());
     }
 
+    @Test
+    @DisplayName("Quando atribuir valor vazio ao nome, estourar exceção IllegalArgumentException com mensagem 'Nome do cargo não pode ser vazio'")
+    void deveLancarExcecaoQuandoAtribuirValorVazioAoNome(){
+        // Arrange
+        Role role = new Role();
 
+        // Act + Assert
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            role.setName("");
+        });
+
+        assertEquals("Nome do cargo não pode ser vazio", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Quando atribuir valor com mais de 50 caracteres ao nome, estourar exceção IllegalArgumentException com mensagem 'Nome do cargo não deve conter mais que 50 caracteres'")
+    void deveLancarExcecaoQuandoAtribuirValorMaiorDe50CaracteresAoNome(){
+        // Arrange
+        Role role = new Role();
+
+        // Act + Assert
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            role.setName("Teste aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        });
+
+        assertEquals("Nome do cargo não deve conter mais que 50 caracteres", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Quando atribuir valor com mais de 3000 caracteres a detalhes, estourar exceção IllegalArgumentException com mensagem 'Detalhes não deve conter mais que 3000 caracteres'")
+    void deveLancarExcecaoQuandoAtribuirValorMaiorDe3000CaracteresADetalhes(){
+        // Arrange
+        Role role = new Role();
+
+        StringBuilder detailBuilder = new StringBuilder();
+        for (int i = 0; i < 3005; i++) {
+            detailBuilder.append(i);
+        }
+        String detail = detailBuilder.toString();
+
+        // Act + Assert
+        String finalDetail = detail;
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            role.setDetail(finalDetail);
+        });
+
+        assertEquals("Detalhes não deve conter mais que 3000 caracteres", exception.getMessage());
+    }
 }
