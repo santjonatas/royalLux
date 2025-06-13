93bbc5bea92a04f0d486912e04787665d93213a7
Jônatas Santos <jonatas.santana@qwasolucoes.com.br>
2025-06-13 09:07:27 -0300

    testes em materialIncrementQuantityUseCase

---
+++ src/test/java/jonatasSantos/royalLux/core/application/usecases/material/MaterialIncrementQuantityUseCaseImplTest.java
@@ -0,0 +1,73 @@
+package jonatasSantos.royalLux.core.application.usecases.material;
+
+import jakarta.persistence.EntityNotFoundException;
+import jonatasSantos.royalLux.core.application.contracts.repositories.MaterialRepository;
+import jonatasSantos.royalLux.core.application.models.dtos.material.MaterialIncrementQuantityUseCaseInputDto;
+import jonatasSantos.royalLux.core.application.models.dtos.material.MaterialIncrementQuantityUseCaseOutputDto;
+import jonatasSantos.royalLux.core.domain.entities.Material;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+class MaterialIncrementQuantityUseCaseImplTest {
+
+    @Mock
+    private MaterialRepository materialRepository;
+
+    @InjectMocks
+    private MaterialIncrementQuantityUseCaseImpl materialIncrementQuantityUseCase;
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    @DisplayName("Quando não existir material a ser incrementado com o mesmo id, estourar exceção EntityNotFoundException com mensagem 'Material é inexistente'")
+    void deveLancarExcecaoQuandoNaoExistirMaterialASerIncrementado() {
+        // Arrange
+        MaterialIncrementQuantityUseCaseInputDto input = new MaterialIncrementQuantityUseCaseInputDto(3);
+
+        when(materialRepository.findById(String.valueOf(1)))
+                .thenReturn(Optional.empty());
+
+        // Act + Assert
+        EntityNotFoundException exception = assertThrows(EntityNotFoundException.class, () -> {
+            materialIncrementQuantityUseCase.execute(1, input);
+        });
+
+        assertEquals("Material é inexistente", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Deve incrementar material com sucesso e retornar quantidade atual em propriedade currentQuantity do output'")
+    void deveIncrementarMaterialComSucesso() {
+        // Arrange
+        MaterialIncrementQuantityUseCaseInputDto input = new MaterialIncrementQuantityUseCaseInputDto(3);
+
+        Material material = new Material("Tinta de cabelo", "Tinta vermelha", BigDecimal.valueOf(40), 5);
+
+        when(materialRepository.findById(String.valueOf(1)))
+                .thenReturn(Optional.of(material));
+
+        // Act
+        MaterialIncrementQuantityUseCaseOutputDto output = materialIncrementQuantityUseCase.execute(1, input);
+
+        // Assert
+        assertNotNull(output);
+        assertEquals(8, output.currentQuantity());
+        verify(materialRepository).save(any(Material.class));
+        verify(materialRepository, times(1)).findById(any(String.class));
+        verify(materialRepository, times(1)).save(any(Material.class));
+    }
+}
